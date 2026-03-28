5a2af61c0f8c6fd72aef280002ca919af834553f
Verdial <99362544+wakeupver@users.noreply.github.com>
2026-03-28 20:20:47 +0700

    Add new system IDs and reorganize existing ones

--- retrograde-app-shared/src/main/java/com/swordfish/lemuroid/lib/library/SystemID.kt
+++ retrograde-app-shared/src/main/java/com/swordfish/lemuroid/lib/library/SystemID.kt
@@ -2,28 +2,51 @@ package com.swordfish.lemuroid.lib.library
 
 enum class SystemID(val dbname: String) {
     NES("nes"),
+    FDS("fds"),                   // Famicom Disk System
     SNES("snes"),
-    GENESIS("md"),
+    N64("n64"),
     GB("gb"),
     GBC("gbc"),
     GBA("gba"),
-    N64("n64"),
-    SMS("sms"),
-    PSP("psp"),
     NDS("nds"),
-    GG("gg"),
-    ATARI2600("atari2600"),
+    NINTENDO_3DS("3ds"),
+    SMS("sms"),                   // Master System
+    SG1000("sg1000"),             // SG-1000
+    GENESIS("md"),                // Mega Drive / Genesis
+    SEGACD("scd"),                // Mega-CD / Sega CD
+    SEGA_32X("sega32x"),          // 32X
+    GG("gg"),                     // Game Gear
+    SATURN("saturn"),             // Saturn
     PSX("psx"),
-    FBNEO("fbneo"),
-    MAME2003PLUS("mame2003plus"),
-    PC_ENGINE("pce"),
-    LYNX("lynx"),
+    PSP("psp"),
+    ATARI2600("atari2600"),
+    ATARI5200("atari5200"),
     ATARI7800("atari7800"),
-    SEGACD("scd"),
+    LYNX("lynx"),
+    JAGUAR("jaguar"),
+    PC_ENGINE("pce"),
+    SUPER_GRAFX("sgfx"),
+    PC_ENGINE_CD("pcecd"),
+    PC_FX("pcfx"),
     NGP("ngp"),
-    NGC("ngc"),
+    NGC("ngc"),                   // Neo Geo Pocket Color
+    NEO_GEO_CD("neocd"),
     WS("ws"),
     WSC("wsc"),
+    POKEMON_MINI("pokemini"),
+    PANASONIC_3DO("3do"),
+    VIRTUAL_BOY("vb"),
     DOS("dos"),
-    NINTENDO_3DS("3ds"),
+    AMIGA("amiga"),
+    C64("c64"),
+    MSX("msx"),
+    MSX2("msx2"),
+    ZX_SPECTRUM("zxspectrum"),
+    AMSTRAD_CPC("amstradcpc"),
+    COLECOVISION("colecovision"),
+    INTELLIVISION("intv"),
+    VECTREX("vectrex"),
+    ODYSSEY2("odyssey2"),
+    FBNEO("fbneo"),
+    MAME2003PLUS("mame2003plus"),
 }
